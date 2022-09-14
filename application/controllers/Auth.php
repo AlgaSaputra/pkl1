16c8c5f7f753b47acec8273f6d9873cf40c550b9
Snoopy <ardamsing@gmail.com>
2022-09-14 13:47:58 +0700

    view cread user

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -527,48 +527,57 @@ class Auth extends CI_Controller
 				'name' => 'first_name',
 				'id' => 'first_name',
 				'type' => 'text',
+				'class' => 'form-control',
+				// 'placeholder' => 'form-control',
 				'value' => $this->form_validation->set_value('first_name'),
 			];
 			$this->data['last_name'] = [
 				'name' => 'last_name',
 				'id' => 'last_name',
 				'type' => 'text',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('last_name'),
 			];
 			$this->data['identity'] = [
 				'name' => 'identity',
 				'id' => 'identity',
 				'type' => 'text',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('identity'),
 			];
 			$this->data['email'] = [
 				'name' => 'email',
 				'id' => 'email',
 				'type' => 'text',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('email'),
 			];
 			$this->data['company'] = [
 				'name' => 'company',
 				'id' => 'company',
 				'type' => 'text',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('company'),
 			];
 			$this->data['phone'] = [
 				'name' => 'phone',
 				'id' => 'phone',
 				'type' => 'text',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('phone'),
 			];
 			$this->data['password'] = [
 				'name' => 'password',
 				'id' => 'password',
 				'type' => 'password',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('password'),
 			];
 			$this->data['password_confirm'] = [
 				'name' => 'password_confirm',
 				'id' => 'password_confirm',
 				'type' => 'password',
+				'class' => 'form-control',
 				'value' => $this->form_validation->set_value('password_confirm'),
 			];
 
--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,57 +1,78 @@
-<h1><?php echo lang('create_user_heading');?></h1>
-<p><?php echo lang('create_user_subheading');?></p>
-
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/create_user");?>
-
-      <p>
-            <?php echo lang('create_user_fname_label', 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_lname_label', 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
-      </p>
-      
-      <?php
-      if($identity_column!=='email') {
-          echo '<p>';
-          echo lang('create_user_identity_label', 'identity');
-          echo '<br />';
-          echo form_error('identity');
-          echo form_input($identity);
-          echo '</p>';
-      }
-      ?>
-
-      <p>
-            <?php echo lang('create_user_company_label', 'company');?> <br />
-            <?php echo form_input($company);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_email_label', 'email');?> <br />
-            <?php echo form_input($email);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_phone_label', 'phone');?> <br />
-            <?php echo form_input($phone);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_label', 'password');?> <br />
-            <?php echo form_input($password);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_confirm_label', 'password_confirm');?> <br />
-            <?php echo form_input($password_confirm);?>
-      </p>
+<?php 
+$this->load->view('templates/header', $this->data);
+$this->load->view('templates/sidebar');
+?>
 
 
-      <p><?php echo form_submit('submit', lang('create_user_submit_btn'));?></p>
+<div class="row">
+      <div class="card">
+            <?php echo form_open("auth/create_user");?>                
+            <div class="card-body">
+                  <div id="infoMessage"><?php echo $message;?></div>                  
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_fname_label', 'first_name');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($first_name);?>
+                              </div>
+                        </div>
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_lname_label', 'last_name');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($last_name);?>
+                              </div>
+                        </div>                        
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_company_label', 'company');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($company);?>
+                              </div>
+                        </div>
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_phone_label', 'phone');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($phone);?>
+                              </div>
+                        </div>                        
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_identity_label', 'identity');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($identity);?>
+                              </div>
+                        </div>
+                        <!-- echo '<p>';
+                        echo lang('create_user_identity_label', 'identity');
+                        echo '<br />';
+                        echo form_error('identity');
+                        echo form_input($identity);
+                        echo '</p>'; -->
+
+
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_email_label', 'email');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($email);?>
+                              </div>
+                        </div>
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_password_label', 'password');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($password);?>
+                              </div>
+                        </div>
+                        <div class="form-group row">
+                              <label for="fname" class="col-sm-3 control-label col-form-label"><?php echo lang('create_user_password_confirm_label', 'password_confirm');?></label>
+                              <div class="col-sm-9">
+                              <?php echo form_input($password_confirm);?>
+                              </div>
+                        </div>
+                        <div class="col-sm-3 text-end control-label col-form-label">
+                              <?php echo form_submit('submit', lang('create_user_submit_btn'), array('class'=>'btn btn-primary'));?>
+                        </div>
+                  </div>            
+            </div>
+      </div>
+
 
 <?php echo form_close();?>
+
+<?php $this->load->view('templates/footer'); ?>
